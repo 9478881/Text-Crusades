fc599e6ace8c371f4487e73fe907bb7ca6a8fcd8
McJCloud29 <103528777+McJCloud29@users.noreply.github.com>
2022-04-16 12:32:17 -0600

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -1,64 +1,59 @@
 public class Monster{
    
-   public String name;
-   
-   public int damage;
-   
-   public int defense;
-   
-   public int health;
-   
-   public boolean alive;
+   private String name;
+   private int damage;
+   private int defense;
+   private int health;
+   private boolean alive = true;
    
    public Monster( String n, int d, int de, int h){
       
       name = n;
-      
       damage = d;
-      
       defense = de;
-      
       health = h;
       
    }
    
    public int getHealth(){
-   
       return health;
    }
    
    public String getName(){
-   
       return name;
    }
    
    public int getDamage(){
-   
       return damage;
    }
    
-   public void takeDamage(int t){;
-     this.health = health -t;
+   public int getDefense(){
+      return defense;
+   }
+   
+   public void takeDamage(int t){
+      if(t > 0){
+         if(t > defense){
+            this.health = health - (t - defense);
+         }else{
+            health = health - 1;
+         }
+      }else{
+         this.health = health - 0;
+      }
    }
    
    
    public boolean getAlive(){
    
-      return health > 0;
+      return alive;
    }
    
-   public void checkAlive(int h){
-     //error Right here. Won't allow for return inside here. 
-      int health = h;
+   public void checkAlive(){      
       if(health <= 0){
-         System.out.println("Monster is dead");
-         //Once We code Monster activities we need to figure out
-         //how to stop its actions. Otherwise monster will keep attacking.
-         //Maybe in the default switch statement if monsters health is zero.
-         //It stops it. Then it asks you for new areas that you can travel too.
+         alive = false;
        
       }
-     
    }
 
 }
